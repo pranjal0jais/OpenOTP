7655a351f889ff99ace79293c3dab5b17d793d65
Pranjal Jais <pranjaljais2@gmail.com>
2026-05-23 22:09:57 +0530

    Fixed tests and Dockerfile

--- src/test/java/com/pranjal/otp_service/OtpServiceApplicationTests.java
+++ src/test/java/com/pranjal/otp_service/OtpServiceApplicationTests.java
@@ -2,12 +2,16 @@ package com.pranjal.otp_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class OtpServiceApplicationTests {
 
+    @MockitoBean
+    private JavaMailSender javaMailSender;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
